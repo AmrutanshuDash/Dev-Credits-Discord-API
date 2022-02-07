5acdf31a9e4b10319bb78302bffffdd6fd20dcbe
AmrutanshuDash <dash.amrutanshu198@gmail.com>
2022-02-07 19:39:02 +0530

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# Dev-Credits-Discord-API
+The API for communicating with the dev credits bot and the database.
+
+
+## 🛠️ Installation Steps
+
+1. Fork the project
+2. Clone the project, by using the following command
+   ```bash
+   $ git clone https://github.com/AmrutanshuDash/Dev-Credits-Discord-API
+   ```
+3. Navigate to the project directory `cd dev-credits-api`
+4. Install dependencies with `yarn install`
+5. Run `yarn start` to start a local development environment
---
+++ controllers/postCredits.js
@@ -0,0 +1,37 @@
+const devCredits = require("../model/model.js")
+
+const postCredits = (req, res) => {
+    const credit = new devCredits({
+        id: req.body.id,
+        credits: req.body.credits
+    })
+
+    devCredits.countDocuments({ id: req.body.id }, function (err, count) {
+        if (count > 0) {
+            devCredits.findOneAndUpdate(
+                { id: req.body.id },
+                {
+                    $inc: {
+                        credits: req.body.credits
+                    },
+                },
+                { new: true },
+                (err, devCredit) => {
+                    if (err) {
+                        res.send(err);
+                    } else res.json(devCredit);
+                }
+            );
+        }
+        else {
+            credit.save((err, image) => {
+                if (err) {
+                    res.send(err)
+                }
+                res.json(image)
+            })
+        }
+    });
+}
+
+module.exports = postCredits
